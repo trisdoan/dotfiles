80c4921192329f0db5713f482ccd342f5bdcb784
trisdoan <doanminhtri8183@gmail.com>
2024-06-07 14:37:03 +0700

    add post-install script

---
+++ fedorable.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# CREDIT: https://github.com/smittix/fedorable
+
+# Flatpak
+enable_flatpak() {
+	echo "Enabling Flatpak"
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak update -y
+	if [ -f flatpak-install.sh ]; then
+		source flatpak-install.sh
+	else
+		echo "flatpak-install.sh not found"
+	fi
+}
+
+# install softwares
+install_software() {
+	echo "Installing software"
+	if [ -f dnf-packages.txt ]; then
+		sudo dnf install -y $(cat dnf-packages.txt)
+		echo "Software installed"
+	else
+		echo "dnf-packages.txt not found"
+	fi
+}
+
+# Function to install extras
+#install_extras() {
+#    echo "Installing Extras"
+#    sudo dnf groupupdate -y sound-and-video
+#    sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+#    sudo dnf install -y libdvdcss
+#    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+#    sudo dnf install -y lame\* --exclude=lame-devel
+#    sudo dnf group upgrade -y --with-optional Multimedia
+#    sudo dnf config-manager --set-enabled fedora-cisco-openh264
+#    sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
+#    sudo dnf copr enable peterwu/iosevka -y
+#    sudo dnf update -y
+#    sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all terminus-fonts terminus-fonts-console google-noto-fonts-common fira-code-fonts cabextract xorg-x11-font-utils fontconfig
+#    sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+#    notify "All done"
+#}
