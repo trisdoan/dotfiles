3ac73783c0fddd89594ef97eed2649c61bad453c
trisdoan <doanminhtri8183@gmail.com>
2024-06-09 15:32:04 +0700

    add kitty and some plugins

--- .config/nvim/detect_theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
-
-if [[ "$THEME" == *"prefer-dark"* ]]; then
-	echo "dark"
-else
-	echo "light"
-fi
--- fedorable.sh
+++ fedorable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# CREDIT: https://github.com/smittix/fedorable
+# CREDIT: https://github.com/smittix/fedorable, https://github.com/geodimm/dotfiles
 
 # Flatpak
 install_flatpak() {
@@ -33,6 +33,47 @@ install_fonts() {
     fc-cache -fv
 }
 
+## FIXME: adapt it to work
+##set -o errexit  # abort on nonzero exitstatus
+##set -o nounset  # abort on unbound variable
+##set -o pipefail # don't hide errors within pipes
+##
+##PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
+##
+##function install_fonts() {
+##  local fonts_dir
+##  case "${PLATFORM}" in
+##  "linux")
+##    group="${USER}"
+##    fonts_dir="${HOME}/.fonts"
+##    ;;
+##  "darwin")
+##    group="staff"
+##    fonts_dir="${HOME}/Library/fonts"
+##    ;;
+##  esac
+##  install -d -m 0755 -o "${USER}" -g "${group}" "${fonts_dir}"
+##
+##  # Font: Hack Nerd Font
+##  # https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Hack/Regular
+##  local install_dir="/tmp/fonts"
+##  rm -rf "${install_dir}" && mkdir -p "${install_dir}"
+##
+##  git clone --quiet --filter=blob:none --sparse "https://github.com/ryanoasis/nerd-fonts.git" "${install_dir}"/nerd-fonts
+##  (
+##    cd "${install_dir}"/nerd-fonts
+##    git sparse-checkout add patched-fonts/Hack
+##    find . -type f -name '*.ttf' ! -name '*Mono*' ! -name '*Propo*' -exec cp "{}" "${fonts_dir}" \;
+##  )
+##
+##  if [[ "${PLATFORM}" == "linux" ]]; then
+##    if ! type -P "fc-cache" >/dev/null; then
+##      sudo apt update && sudo apt install -y fontconfig
+##    fi
+##    sudo fc-cache -f
+##  fi
+##}
+
 # Function to install extras
 #install_extras() {
 #    echo "Installing Extras"
@@ -51,6 +92,41 @@ install_fonts() {
 #    notify "All done"
 #}
 
+# FIXME: modify this function
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
+DOTFILES_DIR="${DOTFILES_DIR:=${HOME}/dotfiles}"
+KITTY_CONFIG_DIR="${XDG_CONFIG_HOME:=${HOME}/.config}/kitty"
+
+configure_kitty() {
+  case "${PLATFORM}" in
+  "linux")
+    local kitty_path
+    kitty_path="$(type -P kitty)"
+    sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator "$kitty_path" 60
+    sudo update-alternatives --set x-terminal-emulator "$kitty_path"
+
+    mkdir -p "${HOME}/.local/share/applications/"
+    cp "${HOME}"/.local/kitty.app/share/applications/kitty*.desktop "${HOME}/.local/share/applications/"
+    sed -i "s|Icon=kitty|Icon=/home/${USER}/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" "${HOME}"/.local/share/applications/kitty*.desktop
+    sed -i "s|Exec=kitty|Exec=/home/${USER}/.local/kitty.app/bin/kitty|g" "${HOME}"/.local/share/applications/kitty*.desktop
+    ;;
+
+  "darwin")
+    sudo ln -fs "/Applications/kitty.app/Contents/MacOS/kitty" "${HOME}/bin/"
+    sudo ln -fs "/Applications/kitty.app/Contents/MacOS/kitten" "${HOME}/bin/"
+    ;;
+  esac
+
+  mkdir -p "${KITTY_CONFIG_DIR}"
+  ln -fs "${DOTFILES_DIR}"/kitty/* "${KITTY_CONFIG_DIR}/"
+
+  kitty +kitten themes --config-file-name=themes.conf Catppuccin-Macchiato
+}
+
 ##
 # Main
 ##
@@ -61,4 +137,6 @@ elif [[ "$1" == "install_flatpak" ]]; then
   install_flatpak
 elif [[ "$1" == "install_fonts" ]]; then
   install_fonts
+elif [[ "$1" == "configure_kitty" ]]; then
+  configure_kitty
 fi
