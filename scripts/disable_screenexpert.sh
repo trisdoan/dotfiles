13a84ca5706698115239d9eb562cb98c57b1824f
trisdoan <doanminhtri8183@gmail.com>
2024-06-22 16:48:18 +0700

    disable asus screenexpert

--- scripts/disable_screenexpert.sh
+++ scripts/disable_screenexpert.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Disable HDMI-1 output
-xrandr --output HDMI-1 --off
+
+if type "xrandr" >/dev/null; then
+  XRANDR_OUTPUT=$(xrandr)
+
+  if echo "$XRANDR_OUTPUT" | grep -q "HDMI-1 connected 1080x2160+1920+0"; then
+    xrandr --output HDMI-1 --off
+  fi
+fi
