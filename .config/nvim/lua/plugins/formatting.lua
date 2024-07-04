7b8f1e9632a9a2ee186140db6a8f740ecde9b5e0
trisdoan <doanminhtri8183@gmail.com>
2024-07-04 17:13:43 +0700

    imp: nvim - add formatter for js

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -4,7 +4,9 @@ return {
     opts = function()
       local opts = {
         formatters_by_ft = {
-          xml = { "xmllint" },
+          xml = { "xmllint"},
+          python = { "black", "flake8", "ruff"},
+          javascript = {"prettier"},
         },
       }
       return opts
