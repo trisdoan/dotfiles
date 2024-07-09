8abfeb63477f765d993be75f60641e094685d070
trisdoan <doanminhtri8183@gmail.com>
2024-07-09 09:11:16 +0700

    upd: nvim - update python formatters

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -5,7 +5,7 @@ return {
       local opts = {
         formatters_by_ft = {
           xml = { "xmllint"},
-          python = { "black", "flake8", "ruff"},
+          python = { "black", "flake8"},
           javascript = {"prettier"},
         },
       }
