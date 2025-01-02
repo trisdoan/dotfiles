77b299207078af9e87ea89dd4ab96acccd8e7315
trisdoan <doanminhtri8183@gmail.com>
2025-01-02 09:09:28 +0700

    upd: add mergiraf and some coding plugins for nvim

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -68,5 +68,15 @@ return {
   -- Docstring generator
   {
     "kkoomen/vim-doge",
+    lazy = false,
+    keys = {
+      {
+        "<leader>cp",
+        function()
+          vim.api.nvim_command("DogeGenerate")
+        end,
+        desc = "doge generate"
+      },
+    },
   },
 }
